f4cdf4330ddbf4991699ffc4fd31648eafb54915
Averoli <afanasenkavolha@gmail.com>
2022-09-30 11:49:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "react-query";
+import TodoList from "./features/todos/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+      (res) => res.json()
+    )
   );
-}
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    // <div>
+    //   <h1>{data.name}</h1>
+    //   <p>{data.description}</p>
+    //   <strong>😎{data.subscribers_count}</strong>{" "}
+    //   <strong>✨{data.stargazers_count}</strong>{" "}
+    //   <strong>💋{data.forks_count}</strong>
+    // </div>
+    <TodoList />
+  );
+};
 
 export default App;
